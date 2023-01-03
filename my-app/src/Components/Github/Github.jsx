6183890f7d0e70deeea5cfa01b62fc13ab70901e
Rsaikiran92 <saikiran92rk@gmail.com>
2023-01-03 23:53:37 +0530

    done

--- my-app/src/Components/Github/Github.jsx
+++ my-app/src/Components/Github/Github.jsx
@@ -8,6 +8,11 @@ function Github(){
         <h2 className="githubheading">GitHub Calendar</h2>
     <GitHubCalender username="Rsaikiran92" style={{marginBottom:"30px",width:"100%" ,margin:"auto"}} />
     </div> 
+    <img
+    
+    src="https://github-readme-stats.vercel.app/api?username=Rsaikiran92&show_icons=true&hide=&count_private=true&title_color=10b981&text_color=000000&icon_color=ef4444&bg_color=ffffff&hide_border=true&show_icons=true"
+    alt="Rsaikiran92 GitHub stats" />
+    <img align="center"  src="https://github-readme-streak-stats.herokuapp.com/?user=Rsaikiran92&"  alt="rsaikiran92" />
     </div>
 )
 }
